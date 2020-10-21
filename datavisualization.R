1e50dc858224b122eab5945466901d73b1c8d1e9
@enriquezcanto <yenriquez@ucss.edu.pe>
2020-10-21 09:17:15 -0500

    se añadió una nueva línea de código

--- datavisualization.R
+++ datavisualization.R
@@ -10,4 +10,7 @@ filter(gapminder, year == 1962) %>%
   ggplot(aes(fertility, life_expectancy)) +
   geom_point()
  
- 
+#-------------------------------------------------------------------------------
+filter(gapminder, year == 1962) %>%
+  ggplot( aes(fertility, life_expectancy, color = continent)) +
+  geom_point() 
