ed029880229930a23c6267ab12c05bbdd6976dd3
@enriquezcanto <yenriquez@ucss.edu.pe>
2020-10-21 09:09:56 -0500

    Se añadió un script con un gráfico de dispersión

---
+++ datavisualization.R
@@ -0,0 +1,13 @@
+#Data visualization
+
+library(tidyverse)
+library(dslabs)
+data(gapminder)
+gapminder %>% as_tibble()
+
+#---------------------------------------------------------------
+filter(gapminder, year == 1962) %>%
+  ggplot(aes(fertility, life_expectancy)) +
+  geom_point()
+ 
+ 
